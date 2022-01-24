43feb84adec8b88e407618ea7ee78c09ae5e39eb
andyrod <andyrod209@gmail.com>
2022-01-23 22:58:28 -0800

    I've added a static file for any other things such as images,css, or java.

--- superhero_project/superheroes/models.py
+++ superhero_project/superheroes/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Superhero(models.Model):
     name = models.CharField(max_length=50)
+    header_image = models.ImageField(null=True, blank=True, upload_to="images/")
     alter_ego = models.CharField(max_length=50)
     primary_ability = models.CharField(max_length=50)
     secondary_ability = models.CharField(max_length=50)
